7859ce3b5b46fde2bd66507c3478691130347861
shobhit7098 <20prince22@gmail.com>
2026-07-09 17:03:08 +0530

    added vercel URL

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://authentication-system-henna.vercel.app']
 
 app.use(express.json());
 app.use(cookieParser());
